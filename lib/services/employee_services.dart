697f7575b8c652b6e4633e0e4b00fc69619dbc3f
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-04-27 12:23:21 +0530

    employee services

--- lib/services/employee_services.dart
+++ lib/services/employee_services.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class EmployeeServices {
@@ -7,6 +9,7 @@ class EmployeeServices {
     var response = await http.get(Uri.parse(baseUrl + "users?page=2"));
     if (response.statusCode == 200) {
       var data = response.body;
+      var decodedData = jsonDecode(data);
     }
   }
 }
