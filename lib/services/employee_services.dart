19f93adeda680c96969290c86b93fcd4f3eaf31f
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-04-29 12:23:21 +0530

    employee services

--- lib/services/employee_services.dart
+++ lib/services/employee_services.dart
@@ -6,12 +6,16 @@ class EmployeeServices {
   String baseUrl = "https://reqres.in/api/";
 
   getAllEmployeeData() async {
-    var response = await http.get(Uri.parse(baseUrl + "users?page=2"));
-    if (response.statusCode == 200) {
-      var data = response.body;
-      var decodedData = jsonDecode(data);
+    try {
+      var response = await http.get(Uri.parse(baseUrl + "users?page=2"));
+      if (response.statusCode == 200) {
+        var data = response.body;
+        var decodedData = jsonDecode(data);
 
-      print(data);
+        print(data);
+      }
+    } catch (e) {
+      throw Exception(e.toString());
     }
   }
 }
