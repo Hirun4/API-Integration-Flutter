e98ed2dc1fe06918c7345be778498537daf5093e
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-05-02 12:23:21 +0530

    employee screen

--- lib/screens/employee_screen.dart
+++ lib/screens/employee_screen.dart
@@ -1,3 +1,4 @@
+import 'package:api_integration_flutter/services/employee_services.dart';
 import 'package:flutter/material.dart';
 
 class EmployeeScreen extends StatelessWidget {
@@ -8,7 +9,9 @@ class EmployeeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Employee Data"),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
+        actions: [IconButton(onPressed: () {
+          EmployeeServices().getAllEmployeeData()
+        }, icon: Icon(Icons.refresh))],
       ),
     );
   }
