875136b567ff5f7fca79cae393cf179f70d0aea1
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-05-09 12:23:21 +0530

    emp service

--- lib/services/employee_services.dart
+++ lib/services/employee_services.dart
@@ -14,6 +14,8 @@ class EmployeeServices {
         var employees = decodedData['data'];
 
         print(employees);
+
+        for (var employee in employees) {}
       }
     } catch (e) {
       throw Exception(e.toString());
