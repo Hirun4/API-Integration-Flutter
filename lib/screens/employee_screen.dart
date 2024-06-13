1553a8852c6a3a583926335172744c4b707be57f
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-06-13 12:23:21 +0530

    subtitle

--- lib/screens/employee_screen.dart
+++ lib/screens/employee_screen.dart
@@ -38,7 +38,7 @@ class EmployeeScreen extends StatelessWidget {
                       ),
                       title: Text(
                           "${data[index].firstName} ${data[index].lastName}"),
-                      subtitle: Text(data),
+                      subtitle: Text(data[index].email!),
                     );
                   });
             }
